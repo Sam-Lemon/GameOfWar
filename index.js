f9e53631f3dd52c84f3aea929281f7431d237c39
Sam-Lemon <theatrewhisper@gmail.com>
2024-05-01 15:37:56 -0700

    added createShuffledDeck method in the Deck class

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 class Card {
-    constructor (suit, rank, value) {
+    constructor (suit, rank) {
         this.suit = suit;
         this.rank = rank;
-        this.value = value;
     }
 
 };
 
-class Deck {
-    constructor () {
-        this.cards = [];
+class Deck {                //THE CODE WITHIN THIS CLASS WORKS, BUT IM NOT SURE ABOUT ONCE ITS IN THE CLASS
+    constructor (card) {
+        this.card = card
     }
 
-    createGameDeck() {
+    createShuffledDeck() {
+        //Fisher-Yates shuffle 
+        const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+        const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-        this.suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
-        this.ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-        this.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        let shuffledDeck = [];      //this is the array the created and shuffled deck will go into
 
-        for(let s = 0; s < suits.length; s++) {     //iterating through the suits array
-            for(let r = 0; r < ranks.length; r++) {     //iterating through the ranks array
-                this.cards.push(new Card(suits[s], ranks[r], values[v]));    //creates a new card and assigns it a rank and suit
+        for (let suit in suits) {
+            for (let rank in ranks) {
+                shuffledDeck.push(ranks[rank] + ' of ' + suits[suit])
             }
         }
-    
+        //Takes a rank from the ranks array and a suit from the suits array and concatenates them. This creates the deck.
+
+        for (let i = shuffledDeck.length -1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+        }
+
+        //i starts at the end of the array and iterates through in descending order. Math.floor rounds down, Math.random returns a random number.
+        //And then the   [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]]; bit takes it all and flips it. So there's two
+        //moments of shuffling, j being a random number, and then the switching of the order of i and j at the end.
     }
+
+
 };
 
 
@@ -44,6 +55,9 @@ class Game {
 
 /*******************************************************************************************************/
 
+
+
+/********************************************************************** */
 //The face cards create a little bit of a problem as they don't have numbers per say, 
 //so I used a switch to take their numerical value and return the face value.
 // switch() {
@@ -58,4 +72,18 @@ class Game {
 //     default:
 //         return 'Oops try again.'
 // }
+
+// createGameDeck() {
+
+//     this.suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+//     this.ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+//     this.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+//     for(let s = 0; s < suits.length; s++) {     //iterating through the suits array
+//         for(let r = 0; r < ranks.length; r++) {     //iterating through the ranks array
+//             cards.push(new Card(suits[s], ranks[r], values[v]));    //creates a new card and assigns it a rank and suit
+//         }
+//     }
+// }
+
 
